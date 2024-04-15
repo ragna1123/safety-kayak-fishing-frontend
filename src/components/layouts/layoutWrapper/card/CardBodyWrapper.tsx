71e2d2131c3ee80c6c3a4c56481ad0b16e9428cf
hiroki haga <hirokihaga@hirokinoMacBook-Pro.local>
2024-04-15 22:55:50 +0900

    マップのピンを指して天気を取得実装

--- src/components/layouts/layoutWrapper/card/CardBodyWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function CardBodyWrapper({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <div className="card-body">{children}</div>;
-}
--- src/components/ui-elements/card/CardTitleH2.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-type CardTitleH2Props = {
-  title: string;
-};
-
-export default function CardTitleH2(props: CardTitleH2Props) {
-  return <h2 className="card-title text-center mb-2">{props.title}</h2>;
-}
